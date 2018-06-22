d4110dd952397102e35e72e67cc058e48e92ff3e
Josh Storm <joshua_taljaard@outlook.com>
2018-06-22 14:16:57 +0100

    added .state Index & home

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,18 @@
 function Router($stateprovider, $urlRouterProvider) {
+  $stateprovider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
 
-  
+    .state('placesIndex', {
+      url: '/places',
+      templateUrl: './veiws/places/index.html',
+      controller: 'PlacesIndexCtrl'
+    })
+
+  $urlRouterProvider.otherwise('/');
 }
+
+export default Router;
