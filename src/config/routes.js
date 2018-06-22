2d36331878920cd6901e88549297d26a908fdaf4
Josh Storm <joshua_taljaard@outlook.com>
2018-06-22 14:41:51 +0100

    finished src-config-routesjs

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,12 +5,40 @@ function Router($stateprovider, $urlRouterProvider) {
       templateUrl: 'views/home.html',
       controller: 'HomeCtrl'
     })
-
     .state('placesIndex', {
       url: '/places',
       templateUrl: './veiws/places/index.html',
       controller: 'PlacesIndexCtrl'
+      // params:{ search: null }
     })
+    .state('placesShow', {
+      url: '/places/:id',
+      templateUrl: '.views/places/show.html',
+      controller: 'PlacesShowCtrl'
+    })
+    .state('placesEdit', {
+      url: '/places/:id/edit',
+      templateUrl: '.views/places/edit.html',
+      controller: 'PlacesEditCtrl'
+      // resolve: { secureState }
+    })
+    .state('placesNew', {
+      url: '/places/new',
+      templateUrl: '.views/places/new.html',
+      controller: 'PlacesNewCtrl'
+      // resolve: { secureState }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'views/auth/login.html',
+      controller: 'AuthLoginCtrl'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'views/auth/register.html',
+      controller: 'AuthRegisterCtrl'
+    });
+
 
   $urlRouterProvider.otherwise('/');
 }
