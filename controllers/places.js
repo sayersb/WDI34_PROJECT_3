752d7c0105e7c343d1d9470bd027e95770c605de
Josh Storm <joshua_taljaard@outlook.com>
2018-06-22 11:41:11 +0100

    working on places.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,1 +1,29 @@
 const Place = require('../model/place');
+
+function indexRoute(req, res, next){
+  Place.find()
+    .then(places => res.json(places))
+    .catch(next);
+
+}
+
+function showRoute(req, res, next) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+
+  index: indexRoute
+
+};
