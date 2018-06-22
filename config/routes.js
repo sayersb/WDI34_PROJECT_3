4fa27d46c3592c304a589f024f107015f81dc548
sayersb <bradleysayers1@gmail.com>
2018-06-22 11:40:31 +0100

    config folder completed with routes.js and environment.js BE

---
+++ config/routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const places = require('../controllers/places');
+const auth = require('../controllers/auth');
+const darksky = require('../controllers/darksky');
+const secureRoute = require('../lib/secureRoute');
+
+
+router.route('/places')
+  .get(places.index)
+  .post(secureRoute, places.create);
+
+router.route('/places/:id')
+  .get(places.show)
+  .put(secureRoute, places.update)
+  .delete(secureRoute, places.delete);
+
+router.post('/places/:id/comments', secureRoute, places.commentCreate);
+router.delete('/places/:id/comments/:commentId', secureRoute, places.commentDelete);
+
+router.get('/profile', secureRoute, auth.profile);
+
+router.get('/forecast', darksky.forecast);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
+
+
+
+module.exports = router;
