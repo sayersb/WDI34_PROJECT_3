55e252e0d57ef6105fdfc889db00099a86182534
sayersb <bradleysayers1@gmail.com>
2018-06-22 12:22:45 +0100

    controller places updated

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,4 @@
-const Place = require('../model/place');
+const Place = require('../models/place');
 
 function indexRoute(req, res, next){
   Place.find()
@@ -9,11 +9,13 @@ function indexRoute(req, res, next){
 
 function showRoute(req, res, next){
   Place.findById(req.params.id)
+    .populate('comments.author')
     .then(place => res.json(place))
     .catch(next);
 }
 
 function createRoute(req, res, next) {
+  req.body.owner = req.currentUser;
   Place.create(req.body)
     .then(place => res.json(place))
     .catch(next);
@@ -35,12 +37,39 @@ function deleteRoute(req, res, next) {
 
 }
 
+function commentCreateRoute(req, res, next) {
+  req.body.author = req.currentUser;
+  Place.findById(req.params.id)
+    .populate('comments.author')
+    .then(place => {
+      place.comments.push(req.body);
+      return place.save();
+    })
+    .then(place => res.json(place))
+    .catch(next);
+}
+
+function commentDeleteRoute(req, res, next) {
+  Place.findById(req.params.id)
+    .then(place => {
+      const comment = place.comments.id(req.params.commentId);
+      comment.remove();
+      return place.save();
+    })
+    .then(place => res.json(place))
+    .catch(next);
+
+}
+
+
 module.exports = {
 
   index: indexRoute,
   show: showRoute,
   create: createRoute,
   update: updateRoute,
-  delete: deleteRoute
+  delete: deleteRoute,
+  commentCreate: commentCreateRoute,
+  commentDelete: commentDeleteRoute
 
 };
