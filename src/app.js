c8ef97806261b087ca093380bc0d55bdfc0ee0ea
Antoni Alston <sntonialston@gmail.com>
2018-06-22 15:39:39 +0100

    imported some routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'satellizer';
+import 'angular-messages';
+import 'filepicker-js';
+import 'angular-filepicker/dist/angular_filepicker';
 
+import 'bulma';
+import './scss/style.scss';
+
+import Router from './config/routes';
+import Auth from './config/satellizer';
+import Upload from './config/filepicker';
+
+import MainCtrl from './controllers/main';
+import HomeCtrl from './controllers/home';
+import PlacesIndexCtrl from './controllers/places/index';
+import PlacesShowCtrl from './controllers/places/show';
+import PlacesNewCtrl from './controllers/places/new';
+import PlacesEditCtrl from './controllers/places/edit';
+import AuthLoginCtrl from './controllers/auth/login';
+import AuthRegisterCtrl from './controllers/auth/register';
+
+import bulmaCard from './directives/bulmaCard';
+import googleMap from './directives/googleMap';
+import googleAutocomplete from './directives/googleAutocomplete';
+import filePicker from './directives/filePicker';
+
+angular.module('placesApi', [
+  'ui.router',
+  'satellizer',
+  'ngMessages',
+  'angular-filepicker'
+])
+  .config(Router)
+  .config(Auth)
+  .config(Upload)
+  .controller('MainCtrl', MainCtrl)
+  .controller('HomeCtrl', HomeCtrl)
+  .controller('PlacesIndexCtrl', PlacesIndexCtrl)
+  .controller('PlacesShowCtrl', PlacesShowCtrl)
+  .controller('PlacesNewCtrl', PlacesNewCtrl)
+  .controller('PlacesEditCtrl', PlacesEditCtrl)
+  .controller('AuthLoginCtrl', AuthLoginCtrl)
+  .controller('AuthRegisterCtrl', AuthRegisterCtrl)
+  .directive('bulmaCard', bulmaCard)
+  .directive('googleMap', googleMap)
+  .directive('googleAutocomplete', googleAutocomplete)
+  .directive('filePicker', filePicker);
