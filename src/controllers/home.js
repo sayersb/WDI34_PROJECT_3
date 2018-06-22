bf736fd141a3e286ac84615166ab47aa53cc9337
Antoni Alston <sntonialston@gmail.com>
2018-06-22 16:24:36 +0100

    added homepage and main .js files

---
+++ src/controllers/home.js
@@ -0,0 +1,12 @@
+function HomeCtrl($scope, $state) {
+  $scope.query = '';
+
+  $scope.$watch('query', () => console.log($scope.query));
+
+  $scope.searchPlaces = function() {
+    $state.go('placesIndex', { search: $scope.query });
+  };
+
+}
+
+export default HomeCtrl;
