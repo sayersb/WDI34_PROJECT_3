dd6e6d6c0068a9799f4e55f4a12a435e0c185852
Josh Storm <joshua_taljaard@outlook.com>
2018-06-22 11:45:43 +0100

    finished controller-places.js

--- controllers/places.js
+++ controllers/places.js
@@ -7,23 +7,40 @@ function indexRoute(req, res, next){
 
 }
 
-function showRoute(req, res, next) {
-  
+function showRoute(req, res, next){
+  Place.findById(req.params.id)
+    .then(place => res.json(place))
+    .catch(next);
 }
 
+function createRoute(req, res, next) {
+  Place.create(req.body)
+    .then(place => res.json(place))
+    .catch(next);
+}
 
+function updateRoute(req, res, next) {
+  Place.findById(req.params.id)
+    .then(place => place.set(req.body))
+    .then(place => place.save())
+    .then(place => res.json(place))
+    .catch(next);
+}
 
+function deleteRoute(req, res, next) {
+  Place.findById(req.params.id)
+    .then(place => place.remove())
+    .then(() => res.sendStatus(204))
+    .catch(next);
 
-
-
-
-
-
-
-
+}
 
 module.exports = {
 
-  index: indexRoute
+  index: indexRoute,
+  show: showRoute,
+  create: createRoute,
+  update: updateRoute,
+  delete: deleteRoute
 
 };
