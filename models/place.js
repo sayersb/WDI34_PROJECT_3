3ce86727812899e571d7626dbbfe5edaa11cc85c
Antoni Alston <sntonialston@gmail.com>
2018-06-26 10:40:39 +0100

    voting is working now. Still need to limit users to one vote per article

--- models/place.js
+++ models/place.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   content: { type: String, required: true },
   author: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  rating: { type: Number, min: 1, max: 5 }
+  rating: { type: Number, min: 1, max: 5 },
+  votes: {
+    count: { type: Number, default: 0}
+  }
 });
 
 const placeSchema = new mongoose.Schema({
