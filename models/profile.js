1bcadce43a9cb234aa6b2050ff6ac6f7720eca71
Josh Storm <joshua_taljaard@outlook.com>
2018-06-28 13:13:39 +0100

    minor changes made to user-profile

--- models/profile.js
+++ models/profile.js
@@ -5,8 +5,7 @@ const profileSchema = new mongoose.Schema({
   image: {type: String, required: true },
   about: {type: String, required: true },
   location: {
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true }
+
   }
 
 });
--- src/controllers/profiles/show.js
+++ src/controllers/profiles/show.js
@@ -9,6 +9,20 @@ function ProfilesShowCtrl($scope, $http, $state) {
     })
       .then(() => $state.go('profilesIndex'));
   };
+  $http({
+    method: 'GET',
+    url: `/api/profiles/${$state.params.id}`
+  })
+    .then(res => {
+      $scope.place = res.data;
+
+      return $http({
+        method: 'GET',
+        url: '/api/forecast',
+        params: { lat: $scope.profile.location.lat, lng: $scope.profile.location.lng }
+      });
+    })
+    .then(res => $scope.forecast = res.data);
 
 
 }
